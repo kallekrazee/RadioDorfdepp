3ff76d9ec907f94d4550441bcdaad3514dde180a
StPercy <stefanpertschi@gmail.com>
2022-01-06 18:06:22 +0100

    got the tracks, got the randomness, got the onceClickStuff and I got the thing going that you can only interact with the site once for clicking the button...seems pretty close to finish

--- js/script.js
+++ js/script.js
@@ -1,17 +1,28 @@
-  function toDo(){
-    changePicture(); selectRandom();
-    
-    }
-
-// The Stuff to do it only once 
-   
-   function doItOnce(){
-      var element=document.getElementById("but01");
-      element.onclick = "";
- }
 
 
-//The stuff to change the pics
+     // The Stuff to make the button NotDBLClickable, but ON(C)E-Clickable:
+
+    const once = f => {
+      let finished = false;
+      return (...args) => {
+        if (!finished) {
+          finished = true;
+          f(...args);
+        }
+      };
+    };
+
+    const but01 = document.getElementById("but01");
+      but01.addEventListener('click', once(toDo).bind(this));
+
+//The Doing of all the Crap, two functions inside to change the pic and to play all the random selected Dorfdeppen-mp3s:
+
+function toDo(){
+  changePicture(); selectRandom(); 
+  document.getElementById("but01").disabled=true; //disabels the button after beeing clicked once :)
+  }
+
+  //The stuff to change the pics
 
   var xchange = true;
   
@@ -31,7 +42,7 @@
 
 var lastSong = null;
 var selection = null;
-var playlist = ["sounds/Der Trüffelo.mp3", "sounds/Ehrlicher Erwin.mp3", "sounds/Vegas.mp3"]; // List of songs
+var playlist = [,"sounds/100jahre.MP3", "sounds/29 Müll is Gold (RMX).mp3", "sounds/27 Radio Doofkappe.mp3", "sounds/26 Silbencrash.mp3" ,"sounds/25 Jogging.mp3", "sounds/22 Die Zeit.mp3", "sounds/16 Denk nich nach.mp3", "sounds/12 Krawall im All.mp3", "sounds/Der Trüffelo.mp3", "sounds/Ehrlicher Erwin.mp3", "sounds/Vegas.mp3", "sounds/01 Chantal.mp3", "sounds/01 r doppel digga das ist der sh.mp3","sounds/3 zu 0.mp3","sounds/07 scheiss dein Hirn aus.mp3",  ]; // List of songs
 var player = document.getElementById("audioplayer"); // Get audio element
 
 
